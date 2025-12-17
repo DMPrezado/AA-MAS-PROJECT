9142fecf1f2e9961843b3cc17d12ed269d256354
David Prezado <65094193+DMPrezado@users.noreply.github.com>
2025-12-17 12:30:33 +0000

    Entities File

--- Entities.py
+++ Entities.py
@@ -1,38 +1,55 @@
 # Entities.py
-from Registry import register
+from MapImporter.Registry import register
 
-@register("W")
-class Wall:
-    pass
+'''Agent entity should not be here because of its complexity'''
 
-@register("F")
-class Fireplace:
-    pass
 
+'''WARNING: REWARDS NEED TO BE ON AN ACCESSIBLE PLACE TO CHANGE THEM EASILY'''
+'''WARNING: COLORS OF EACH CLASS NEED TO BE DEFINED ON THE CLASS ITSELF'''
+
+
+# -----------------------
+# Lighthouse
+# -----------------------
+@register("L")
+class LightHouse: 
+    def __init__(self,coord):
+        self.coord = coord
+        self.reward = 100
+
+
+# -----------------------
+# Foraging
+# -----------------------
 @register("N")
 class Nest:
-    pass
+    def __init__(self,coord):
+        self.coord = coord
+        self.reward = 100
 
 @register("R")
 class Resource:
-    pass
+    def __init__(self,coord):
+        self.coord = coord
+        self.reward = 50
 
-@register("A")
-class Agent:
-    pass
 
-@register("#")
-class Obstacle:
-    pass
 
-@register("L")
-class LightHouse: 
-    pass
+# -----------------------
+# Common Entities
+# -----------------------
+@register("W")
+class Wall:
+    def __init__(self,coord):
+        self.coord = coord
+        self.reward = -10
 
-@register("B")
-class Limit:
-    pass
+@register("F")
+class Fireplace:
+    def __init__(self,coord):
+        self.coord = coord
+        self.reward = -20
+
+
+
     
-@register(".")
-class Empty:
-    pass
